df90d569f594c2c500f935bb72ac30e32e356300
Patricia Kennedy <patriciacken@gmail.com>
2024-12-10 13:57:09 -0600

    assignment challenge 1-4 completed

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,12 +1,62 @@
 console.log('***** Music Collection *****')
 // Safe Zone -- Write code below this line
 
+// REF: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array - PK
+// REF: https://developer.mozilla.org/en-US/docs/Learn/JavaScript/First_steps/Arrays - PK
+
+// TASK1: Creating arrays, objects, accessing & assigns values, return - PK
+let myCollection = [];
+
+// Reuse code for adding albums - PK
+function addToCollection(collection, title, artist, yearPublished) {
+  
+  const newAlbum = {
+    title: title,
+    artist: artist,
+    yearPublished: yearPublished,
+  };
+
+  collection.push(newAlbum);
+
+  return newAlbum;
+}
+
+// TASK2: Using and testing addToCollection function: - PK
+
+console.log(addToCollection(myCollection, 'Trusting God', 'Sam James', 2021));
+console.log(addToCollection(myCollection, 'Obey', 'Mark Johnson', 1987));
+console.log(addToCollection(myCollection, 'God is able', 'Grace Miller', 2021));
+console.log(addToCollection(myCollection, 'Never', 'Emily Smith', 2021));
+console.log(addToCollection(myCollection, 'What a world', 'Jay Martinez', 2024));
+console.log(addToCollection(myCollection, 'Trusting God Always', 'Sophia Anderson', 2019));
+
+console.log(myCollection);
+
+
+// TASK3:
+
+function showCollection(collection) {
+  for (const album of collection) {
+    console.log(`${album.title} by ${album.artist}, published in ${album.yearPublished}`);
+  }
+}
+
+console.log(showCollection(myCollection));
+
+
+// TASK4:Use and test the showCollection function.
+// showCollection(myCollection);
 
 
 
 
 
 
+
+
+
+
+//--------------------------------- NO CODE ZONE -----------------------------------------  - PK
 
 
 // PLEASE DO NOT MODIFY THIS. Just leave it down here at the bottom. Think of it
